5010194c449840e35f00cd67ae769c6055828e2e
Aman <amanphankhu@gmail.com>
2025-10-04 16:49:06 +0530

    reset

--- frontend/prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-  trailingComma: 'es5',
-  tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  printWidth: 80,
-};
